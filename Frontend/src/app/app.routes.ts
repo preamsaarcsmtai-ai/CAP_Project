487722b48ca732febb78702af57aadd81b9d51c5
jeeva <jeeva-smt@gmailoutlook.com>
2025-09-09 13:10:07 +0530

    routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Layout } from './layout/layout';
 import { Superadmin } from './superadmin/superadmin';
 import { Institution } from './pages/institution/institution';
 import { Users } from './pages/users/users';
+import { Students } from './pages/students/students';
 
 
 export const routes: Routes = [
@@ -16,5 +17,6 @@ export const routes: Routes = [
       {path:'users', component:Users},
       { path: '', redirectTo: 'superadmin', pathMatch: 'full' }
     ]
-  }
+  },
+  {path:'students',component:Students}
 ];
