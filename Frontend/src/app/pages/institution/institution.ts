faa23284a9f9804ea409cd0a4fd5f6d5d62c5ead
jeeva <jeeva-smt@gmailoutlook.com>
2025-09-11 18:05:46 +0530

    institution dashboard developed

--- Frontend/src/app/pages/institution/institution.ts
+++ Frontend/src/app/pages/institution/institution.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-institution',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './institution.html',
   styleUrl: './institution.css'
 })
 export class Institution {
 
+  institution =[
+    {
+      name:'MIT College of Engineering',college:'Engineering',location:'America',users:'users 5',mail:'mit@gmail.com',phone:'+91 987654321',website:'https://mit.College',admin:'John',status: 'active'
+    } ,
+    {
+      name:'MMC College of Arts',college:'Arts and science',location:'Theni',users:'users 4',mail:'mmc@gmail.com',phone:'+91 34567654',website:'https://mmc.College',admin:'Ganesh',status: 'pending' 
+    }, 
+    {
+      name:'SRM College of Engineering',college:'Engineering',location:'Chennai',users:'users 10',mail:'srm@gmail.com',phone:'+91 432156784',website:'https://srm.College',admin:'Raj',status: 'suspended' 
+    }
+  ]
 }
--- Frontend/src/app/pages/users/users.ts
+++ Frontend/src/app/pages/users/users.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.html',
   styleUrl: './users.css'
 })
 export class Users {
+viewAdmin=false;
+    users = [
+    { name: 'John', role: 'Institution Admin', institution: 'MIT College', status: 'active' },
+    { name: 'Ganesh', role: 'Institution Admin', institution: 'MMC College', status: 'pending' },
+    { name: 'Raj', role: 'Institution Admin', institution: 'SRM College', status: 'suspended' }
+  ];
 
+ onView(){
+this.viewAdmin=true;
+ } 
 }
